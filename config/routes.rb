1c7fdf07563e9ebaae5f70f77b7cdc19a6a54791
Anne Costa <anneizaura@gmail.com>
2020-05-27 21:27:46 -0300

    restaurants 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :restaurants, except: [:edit, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#populei meu bd
+
+puts "iniciando seed"
+
+Restaurant.all.each do |r|
+  r.destroy
+end
+puts "foram apagados todos restaurantes"
+
+Restaurant.create(name: 'Sal', address: 'Cidade Jardim', category: 'french')
+
+Restaurant.create(name: 'Chineinbox', address: 'Cidade Jardim', category: 'chinese')
+
+Restaurant.create(name: 'Italy', address: 'Cidade Jardim', category: 'italian')
+
+Restaurant.create(name: 'Arakaki', address: 'Cidade Jardim', category: 'japanese')
+
+Restaurant.create(name: 'Belga', address: 'Cidade Jardim', category: 'belgian')
+
+puts "foram add 5 restaurants"
